8addd2fed248a8fb81575fb82c52d7c41177c6aa
taquocy <taquocitali@gmail.com>
2022-01-11 11:35:08 +0700

    implement share content to Facebook

--- LTDD2/loginscreen/lib/main.dart
+++ LTDD2/loginscreen/lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:loginscreen/widgets/cardCity.dart';
+import 'dart:async';
+import 'package:flutter/services.dart';
 
-import 'widgets/UserCard.dart';
+import 'package:flutter_social_content_share/flutter_social_content_share.dart';
+
+
+
 
 void main() {
   runApp(const MyApp());
@@ -44,8 +48,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  String _platformVersion = 'Unknown';
   final _formKey = GlobalKey<FormState>();
 
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = (await FlutterSocialContentShare.platformVersion)!;
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  /// SHARE ON FACEBOOK CALL
+  shareOnFacebook() async {
+    String? result = await FlutterSocialContentShare.share(
+        type: ShareType.facebookWithoutImage,
+        url: "https://www.apple.com",
+        quote: "captions");
+    print(result);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,189 +91,153 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       // ignore: avoid_unnecessary_ontainercontainers
-      body:
-      GridView.count(
+      body: Form(
+        key: _formKey,
+        child: Container(
+          padding: const EdgeInsets.only(left: 20, right: 20),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // ignore: prefer_const_constructors
+                Text(
+                  'FELLOW4U',
+                  style: const TextStyle(fontSize: 50),
+                ),
+                const SizedBox(height: 30),
 
-        primary: false,
-        padding: const EdgeInsets.all(20),
-        crossAxisSpacing: 30,
-        mainAxisSpacing: 10,
-        crossAxisCount: 3,
-        children: const <Widget>[
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-          CardCity(),
-        ],
-      )
+                // ignore: prefer_const_constructors
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter email!';
+                    }
+                    return null;
+                  },
 
+                  decoration: const InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.add_outlined,
+                        size: 30,
+                      ),
+                      border: OutlineInputBorder(),
+                      hintText: 'Email'),
+                ),
+                const SizedBox(height: 30),
 
+                TextFormField(
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Please enter password ';
+                    }
+                    if(value.length < 6){
+                      return 'Your password must be at lease 6 character';
+                    }
+                    return null;
+                  },
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      suffixIcon: Icon(
+                        Icons.add_outlined,
+                        size: 30,
+                      ),
+                      border: OutlineInputBorder(),
+                      hintText: 'Password'),
+                ),
+                const SizedBox(height: 30),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: OutlinedButton(
+                    child: const Text('Login with Email'),
+                    onPressed: () {
+                      print('Login!');
+                      if (_formKey.currentState!.validate()) {
+                        print('FORM IS OK!!!');
+                        //
+                      }
 
-        // ListView.builder(
-        //     scrollDirection: Axis.horizontal,
-        //     itemCount: 10,
-        //     itemBuilder: (context, index){
-        //       if(index % 2 == 0){
-        //         return Divider(thickness: 5, height: 10);
-        //       }
-        //     return CardCity();
-        // })
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: OutlinedButton(
+                    child: const Text('Login with Facebook'),
+                    onPressed: () async {
+                      print('Login with Facebook!');
 
+                      final LoginResult result =
+                          await FacebookAuth.instance.login(
+                        permissions: ['public_profile', 'email'],
+                      );
 
-        // ListView(
-        //   children: const [
-        //     CardCity(),
-        //     CardCity(),
-        //     CardCity(),
-        //   ],
-        // )
+                      if (result.status == LoginStatus.success) {
+                        // you are logged
+                        print('LOGIN TOKEN!!!!');
+                        print(result.accessToken!.token);
+                        final userData = await FacebookAuth.i.getUserData(
+                          fields: "name,email",
+                        );
+                        prettyPrint(userData);
+                      } else {
+                        print('YYYYY');
+                        print(result.status);
 
-      // Form(
-      //   key: _formKey,
-      //   child: Container(
-      //     padding: const EdgeInsets.only(left: 20, right: 20),
-      //     child: Center(
-      //       child: Column(
-      //         mainAxisAlignment: MainAxisAlignment.center,
-      //         children: [
-      //           // ignore: prefer_const_constructors
-      //           Text(
-      //             'FELLOW4U',
-      //             style: const TextStyle(fontSize: 50),
-      //           ),
-      //           const SizedBox(height: 30),
-      //
-      //           // ignore: prefer_const_constructors
-      //           TextFormField(
-      //             validator: (value) {
-      //               if (value == null || value.isEmpty) {
-      //                 return 'Please enter email!';
-      //               }
-      //               return null;
-      //             },
-      //
-      //             decoration: const InputDecoration(
-      //                 suffixIcon: Icon(
-      //                   Icons.add_outlined,
-      //                   size: 30,
-      //                 ),
-      //                 border: OutlineInputBorder(),
-      //                 hintText: 'Email'),
-      //           ),
-      //           const SizedBox(height: 30),
-      //
-      //           TextFormField(
-      //             validator: (value){
-      //               if(value == null || value.isEmpty){
-      //                 return 'Please enter 222312323123password 222323123132 111111111112323111111111222222221112222';
-      //               }
-      //               if(value.length < 6){
-      //                 return 'Your password must be at lease 6 character';
-      //               }
-      //               return null;
-      //             },
-      //             obscureText: true,
-      //             decoration: const InputDecoration(
-      //                 suffixIcon: Icon(
-      //                   Icons.add_outlined,
-      //                   size: 30,
-      //                 ),
-      //                 border: OutlineInputBorder(),
-      //                 hintText: 'Password'),
-      //           ),
-      //           const SizedBox(height: 30),
-      //           SizedBox(
-      //             width: MediaQuery.of(context).size.width,
-      //             height: 50,
-      //             child: OutlinedButton(
-      //               child: const Text('Login with Email'),
-      //               onPressed: () {
-      //                 print('Login!');
-      //                 if (_formKey.currentState!.validate()) {
-      //                   print('FORM IS OK!!!');
-      //                   //
-      //                 }
-      //
-      //               },
-      //             ),
-      //           ),
-      //           const SizedBox(
-      //             height: 20,
-      //           ),
-      //           SizedBox(
-      //             width: MediaQuery.of(context).size.width,
-      //             height: 50,
-      //             child: OutlinedButton(
-      //               child: const Text('Login with Facebook'),
-      //               onPressed: () async {
-      //                 print('Login with Facebook!');
-      //
-      //                 final LoginResult result =
-      //                     await FacebookAuth.instance.login(
-      //                   permissions: ['public_profile', 'email'],
-      //                 );
-      //
-      //                 if (result.status == LoginStatus.success) {
-      //                   // you are logged
-      //                   print('LOGIN TOKEN!!!!');
-      //                   print(result.accessToken!.token);
-      //                   final userData = await FacebookAuth.i.getUserData(
-      //                     fields: "name,email",
-      //                   );
-      //                   prettyPrint(userData);
-      //                 } else {
-      //                   print('YYYYY');
-      //                   print(result.status);
-      //
-      //                   print(result.message);
-      //                 }
-      //               },
-      //             ),
-      //           ),
-      //           const SizedBox(height: 30),
-      //
-      //           Row(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             crossAxisAlignment: CrossAxisAlignment.center,
-      //             children: [
-      //               IconButton(
-      //                 icon: const Icon(
-      //                   Icons.account_circle,
-      //                   size: 50,
-      //                 ),
-      //                 onPressed: () {},
-      //               ),
-      //               Padding(
-      //                 padding: const EdgeInsets.only(left: 30, right: 30),
-      //                 child: IconButton(
-      //                   icon: const Icon(
-      //                     Icons.account_circle_outlined,
-      //                     size: 50,
-      //                     color: Colors.red,
-      //                   ),
-      //                   onPressed: () {},
-      //                 ),
-      //               ),
-      //               IconButton(
-      //                 icon: const Icon(
-      //                   Icons.account_circle,
-      //                   size: 50,
-      //                 ),
-      //                 onPressed: () {},
-      //               ),
-      //             ],
-      //           )
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
+                        print(result.message);
+                      }
+                    },
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+                RaisedButton(
+                  child: Text('Share to Facebook Link'),
+                  onPressed: () async {
+                    shareOnFacebook();
+                  },
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(
+                        Icons.account_circle,
+                        size: 50,
+                      ),
+                      onPressed: () {},
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.account_circle_outlined,
+                          size: 50,
+                          color: Colors.red,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.account_circle,
+                        size: 50,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
